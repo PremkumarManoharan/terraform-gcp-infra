af53ccaef94eb8ed545aaf5bf30c118716e512f5
PremkumarManoharan <raghavamanoharan.p@northeastern.edu>
2024-02-09 12:30:04 -0500

    Terraform init with GCP provider

---
+++ variables.tf
@@ -0,0 +1,12 @@
+
+variable "region" {
+  type        = string
+  description = "GCP region for all resources."
+  default = "us-east1"
+}
+
+variable "project_name" {
+  type        = string
+  description = "Name of the example project."
+  default = "csye-6225-dev"
+}
