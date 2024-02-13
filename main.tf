a2beaf5956452124e6058e0354307ad8d1fadf9a
PremkumarManoharan <raghavamanoharan.p@northeastern.edu>
2024-02-13 14:17:36 -0500

    added code for dynamic vpc creation

--- main.tf
+++ main.tf
@@ -1,25 +1,25 @@
 resource "google_compute_network" "vpc_network" {
-  name                              = "vpc-network"
-  auto_create_subnetworks           = false
-  routing_mode                      = "REGIONAL"
-  delete_default_routes_on_create   = true 
+  name                              = var.vpc_name
+  auto_create_subnetworks           = var.vpc_auto_create_subnetworks
+  routing_mode                      = var.vpc_routing_mode
+  delete_default_routes_on_create   = var.vpc_delete_default_routes_on_create 
 }
 
-resource "google_compute_subnetwork" "webapp" {
-  name          = "webapp"
-  ip_cidr_range = "10.0.1.0/24"
+resource "google_compute_subnetwork" "webapp_subnet" {
+  name          = var.webapp_subnet_name
+  ip_cidr_range = var.webapp_ip_cidr_range
   network       = google_compute_network.vpc_network.id
 }
 
-resource "google_compute_subnetwork" "db" {
-  name          = "db"
-  ip_cidr_range = "10.0.2.0/24"
+resource "google_compute_subnetwork" "db_subnet" {
+  name          = var.db_subnet_name
+  ip_cidr_range = var.db_ip_cidr_range
   network       = google_compute_network.vpc_network.id
 }
 
 resource "google_compute_route" "webapp-route" {
-  name        = "webapp-route"
-  dest_range  = "0.0.0.0/0"
+  name        =  var.webapp_route_name
+  dest_range  =  var.webapp_route_dest_range
+  next_hop_gateway = var.webapp_route_next_hop_gateway
   network     = google_compute_network.vpc_network.name
-  next_hop_gateway = "default-internet-gateway"
 }
